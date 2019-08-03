96e051b5c3433480a7140a9aa29f9d49c8faadc3
david-reid <david@admiredigital.com>
2019-08-03 06:15:50 +0100

    Added canvas and context to the index.js file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Constants and variables
+let canvas; 
+let context;
+
+
 window.onload = function() {
+    
     console.log('JavaScript Breakout v1.0.0');
+
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
 }
