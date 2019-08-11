1fc07c26026e5171d36d4f1b180eb2cedfeae3f5
david-reid <david@admiredigital.com>
2019-08-11 12:00:33 +0100

    Added the game loop functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Constants and variables
 let canvas; 
 let context;
+const FRAMES_PER_SECOND = 30;
 
 
 window.onload = function() {
@@ -9,4 +10,31 @@ window.onload = function() {
 
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
+
+    resize();
+
+    setInterval(gameLoop, 1000/FRAMES_PER_SECOND);
+}
+
+// Utility functions
+
+function resize() {
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 6;
+}
+
+// Game Loop Functions
+function gameLoop() {
+
+    moveEverything();
+    drawEverything();
+}
+
+function moveEverything() {
+    //console.log('move everything');
+}
+
+function drawEverything() {
+    //console.log('draw everything');
 }
